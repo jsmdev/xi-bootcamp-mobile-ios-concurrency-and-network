bb58b2a73ce0fedf3116c1bb77b12eac9b5c6374
José Sancho <jsancho.dev@gmail.com>
2021-01-20 18:52:31 +0100

    The files for the User and UserDetail modules have been created

---
+++ DiscourseClient/DiscourseClient/Users/UsersDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  UsersDataManager.swift
+//  DiscourseClient
+//
+//  Created by José Sancho on 20/1/21.
+//  Copyright © 2021 Roberto Garrido. All rights reserved.
+//
+
+import Foundation
---
+++ DiscourseClient/DiscourseClient/Users/UsersViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UsersViewController.swift
+//  DiscourseClient
+//
+//  Created by José Sancho on 20/1/21.
+//  Copyright © 2021 Roberto Garrido. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
